802c9aed1dd7b5dd136b69e7b74d9ce6afb8647d
David Lu <davidvlu@gmail.com>
2015-09-19 16:05:00 -0400

    SDF Macro

---
+++ scripts/sdf_xacro.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+import resource_retriever
+import subprocess
+
+def find(package):
+    process = subprocess.Popen(['rospack', 'find', package], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = process.communicate()
+    if len(stderr) > 0:
+        raise Exception(stderr)
+    else:
+        return str(stdout).strip()
+
+s = open(sys.argv[1]).read()
+
+D = {}
+for arg in sys.argv[2:]:
+    if '=' in arg:
+        k,e,v = arg.partition('=')
+        D[k] = v
+
+while '$(' in s:
+    i = s.index('$(')
+    i2 = s.index(')', i)
+    command = s[i+2:i2].split(' ')
+    if command[0]=='find':
+        middle = 'file://' + find(command[1])
+    elif command[0]=='arg':
+        middle = D[command[1]]
+    else:
+        print "could not find command", command[0]
+        exit(-1)        
+    s = s[:i] + middle + s[i2+1:]
+
+print s
